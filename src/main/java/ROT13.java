2a9187ec6c7398ba45dede948395a2cfec29d0d9
asanahmadli <asenahmedli@gmail.com>
2024-04-02 23:15:07 -0400

    Finished

--- src/main/java/ROT13.java
+++ src/main/java/ROT13.java
@@ -1,8 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 import static java.lang.Character.isLowerCase;
 import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
-public class ROT13  {
+public class ROT13 {
 
     char cs;
     char cf;
@@ -13,16 +19,15 @@ public class ROT13  {
     }
 
     ROT13() {
+        this('a','n');
     }
 
 
     public String crypt(String text) throws UnsupportedOperationException {
-
-        return "";
+        return encrypt(text);
     }
 
-    public String encrypt(String f) {
-        String text  = f;
+    public String encrypt(String text) {
         String[] split = text.split(" ");
         StringBuilder sb = new StringBuilder();
         String ch = String.valueOf(cf).toLowerCase();
@@ -31,49 +36,42 @@ public class ROT13  {
         String encrypt = "";
 
         //alphabetSmall
-        for(int i = 'a'; i <='z';i++ ){
-            alphabetSmall+=(char)i;
+        for (int i = 'a'; i <= 'z'; i++) {
+            alphabetSmall += (char) i;
         }
         //alphabetCapital
-        for(int i = 'A'; i <='Z';i++ ){
-            alphabetCapital+=(char)i;
+        for (int i = 'A'; i <= 'Z'; i++) {
+            alphabetCapital += (char) i;
         }
 
         int index = alphabetSmall.indexOf(ch);
         //find sliding alphabet
-        String slidingSmall = alphabetSmall.substring(alphabetSmall.length()-index)+alphabetSmall.substring(0,alphabetSmall.length()-index);
-        String slidingCapital = alphabetCapital.substring(alphabetCapital.length()-index)+alphabetCapital.substring(0,alphabetCapital.length()-index);
+        String slidingSmall = alphabetSmall.substring(alphabetSmall.length() - index) + alphabetSmall.substring(0,index);
+        String slidingCapital = alphabetCapital.substring(alphabetCapital.length() - index) + alphabetCapital.substring(0,index);
 
-        // System.out.println(slidingCapital);
-        // System.out.println(sliding);
-        // System.out.println(slidingSmall);
-        //loop through string
-        for(int s = 0; s<split.length;s++){
+        for (int s = 0; s < split.length; s++) {
 
-            for(int p = 0; p<split[s].length();p++) {
-                if (alphabetSmall.contains(split[s].charAt(p) + "")) {
+            for (int p = 0; p < split[s].length(); p++) {
+                if (Character.isLowerCase(split[s].charAt(p))) {
                     int smallLetterIndex = 0;
-                    for(int m = 0; m<alphabetSmall.length();m++){
-                        if(split[s].charAt(p)==alphabetSmall.charAt(m)){
+                    for (int m = 0; m < alphabetSmall.length(); m++) {
+                        if (split[s].charAt(p) == alphabetSmall.charAt(m)) {
                             smallLetterIndex = m;
                             break;
                         }
                     }
-                    encrypt+=slidingSmall.charAt(smallLetterIndex);
-                }
-                else if (alphabetCapital.contains(split[s].charAt(p) + "")) {
+                    encrypt += slidingSmall.charAt(smallLetterIndex);
+                } else if (Character.isUpperCase(split[s].charAt(p))) {
                     int capitalLetterIndex = 0;
-                    for(int m = 0; m<alphabetCapital.length();m++){
-                        if(split[s].charAt(p)==alphabetCapital.charAt(m)){
+                    for (int m = 0; m < alphabetCapital.length(); m++) {
+                        if (split[s].charAt(p) == alphabetCapital.charAt(m)) {
                             capitalLetterIndex = m;
                             break;
                         }
                     }
-
-                    encrypt+=slidingCapital.charAt(capitalLetterIndex);
-                }
-                else {
-                    encrypt+=split[s].charAt(p);
+                    encrypt += slidingCapital.charAt(capitalLetterIndex);
+                } else {
+                    encrypt += split[s].charAt(p);
                 }
             }//inner loop
             sb.append(encrypt).append(" ");
@@ -88,90 +86,53 @@ public class ROT13  {
         return encrypt(text);
     }
 
-    public static String rotate(String f, Character c) {
-        String text  = "Gb trg gb gur bgure fvqr!";
-        String[] split = text.split(" ");
-        StringBuilder sb = new StringBuilder();
-        String ch = String.valueOf(c).toLowerCase();
-        String alphabetSmall = "";
-        String alphabetCapital = "";
-        String encrypt = "";
-
-        //alphabetSmall
-        for(int i = 'a'; i <='z';i++ ){
-            alphabetSmall+=(char)i;
-        }
-        //alphabetCapital
-        for(int i = 'A'; i <='Z';i++ ){
-            alphabetCapital+=(char)i;
-        }
-
-        int index = alphabetSmall.indexOf(ch);
-        //find sliding alphabet
-        String slidingSmall = alphabetSmall.substring(alphabetSmall.length()-index)+alphabetSmall.substring(0,alphabetSmall.length()-index);
-        String slidingCapital = alphabetCapital.substring(alphabetCapital.length()-index)+alphabetCapital.substring(0,alphabetCapital.length()-index);
-        return slidingCapital;
+    public static String rotate(String s, Character c) {
+        int index = s.indexOf(c);  // changed code to simplified code
+        return s.substring(index) + s.substring(0, index);
     }
 
+
+
+    public  void fileReader() {
+        try{
+            File file = new File("/Users/asan/Desktop/Projects/SimpleCrypt/sonnet18.txt");
+            FileWriter fileWriter = new FileWriter("/Users/asan/Desktop/Projects/SimpleCrypt/sonnet19.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()){
+                String str = scanner.nextLine();
+                fileWriter.write(encrypt(str));
+                fileWriter.append("\n");
+                fileWriter.flush();
+            }
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public  void fileReader1() {
+        try{
+            File file = new File("/Users/asan/Desktop/Projects/SimpleCrypt/sonnet19.txt");
+            FileWriter fileWriter = new FileWriter("/Users/asan/Desktop/Projects/SimpleCrypt/sonnet20.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()){
+                String str = scanner.nextLine();
+                fileWriter.write(encrypt(str));
+                fileWriter.append("\n");
+                fileWriter.flush();
+            }
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     public static void main(String[] args) {
-      String text  = "Gb trg gb gur bgure fvqr!";
-      String[] split = text.split(" ");
-      StringBuilder sb = new StringBuilder();
-      String ch = String.valueOf('n').toLowerCase();
-      String alphabetSmall = "";
-      String alphabetCapital = "";
-      String encrypt = "";
-
-      //alphabetSmall
-      for(int i = 'a'; i <='z';i++ ){
-          alphabetSmall+=(char)i;
-      }
-        //alphabetCapital
-        for(int i = 'A'; i <='Z';i++ ){
-            alphabetCapital+=(char)i;
-        }
-
-        int index = alphabetSmall.indexOf(ch);
-        //find sliding alphabet
-        String slidingSmall = alphabetSmall.substring(alphabetSmall.length()-index)+alphabetSmall.substring(0,alphabetSmall.length()-index);
-        String slidingCapital = alphabetCapital.substring(alphabetCapital.length()-index)+alphabetCapital.substring(0,alphabetCapital.length()-index);
-
-        System.out.println(slidingCapital);
-        // System.out.println(sliding);
-        System.out.println(slidingSmall);
-        //loop through string
-        for(int s = 0; s<split.length;s++){
-
-            for(int p = 0; p<split[s].length();p++) {
-                if (alphabetSmall.contains(split[s].charAt(p) + "")) {
-                    int smallLetterIndex = 0;
-                    for(int m = 0; m<alphabetSmall.length();m++){
-                        if(split[s].charAt(p)==alphabetSmall.charAt(m)){
-                            smallLetterIndex = m;
-                            break;
-                        }
-                    }
-                    encrypt+=slidingSmall.charAt(smallLetterIndex);
-                }
-                else if (alphabetCapital.contains(split[s].charAt(p) + "")) {
-                    int capitalLetterIndex = 0;
-                    for(int m = 0; m<alphabetCapital.length();m++){
-                        if(split[s].charAt(p)==alphabetCapital.charAt(m)){
-                            capitalLetterIndex = m;
-                            break;
-                        }
-                    }
-
-                    encrypt+=slidingCapital.charAt(capitalLetterIndex);
-                }
-                else {
-                    encrypt+=split[s].charAt(p);
-                }
-            }//inner loop
-            sb.append(encrypt).append(" ");
-            encrypt = "";
-        }//outer loop
-
-        System.out.println(sb.toString());
+        ROT13 rot13 = new ROT13();
+        rot13.fileReader();
+        rot13.fileReader1();
     }
+
 }
